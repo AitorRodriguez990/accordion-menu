f20531586de0645a3e4d4c1f31c3bedb54477e9c
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2018-09-29 17:02:52 +0200

    Improve header changing color depending on selected accordion menu and some other changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,38 @@
-(function() {
-  const ACTIVE_CLASS = "c-accordion__title--active";
+const ACTIVE_CLASS = "c-accordion__title--active";
 
+(function() {
   let accordionElements = Array.from(document.querySelectorAll('.c-accordion__title'));
 
   const handleClickAccordionElement = (e) => {
     e.preventDefault();
     
+    // Remove active class from all elements
     accordionElements.forEach(element => {
       element.classList.remove(ACTIVE_CLASS);
     });
 
+    // Add active class to selected element
     e.currentTarget.classList.add(ACTIVE_CLASS);
+
+    // Change header color depending on accordion title color
+    changeHeaderColor(e.currentTarget);
   }
   
   accordionElements.forEach(element => {
     element.addEventListener('click', handleClickAccordionElement)
   });
 })();
+
+const changeHeaderColor = () => {
+  let elementStyles = getComputedStyle(document.querySelector("." + ACTIVE_CLASS)),
+      newStyles = {
+        backgroundColor: elementStyles.backgroundColor,
+        color: elementStyles.color
+      };
+
+  setStylesOnElement(newStyles, document.getElementsByClassName("c-header")[0]);
+};
+
+const setStylesOnElement = function(styles, element){
+  Object.assign(element.style, styles);
+}
