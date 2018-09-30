786e04fd2a4fcaa346ada84584875f1b7404f6cb
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2018-09-30 10:34:32 +0200

    Little refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,44 @@
-const ACTIVE_CLASS = "c-accordion__title--active";
+const ACCORDION_TITLE = "c-accordion__title",
+      ACCORDION_TITLE_ACTIVE = "c-accordion__title--active",
+      HEADER = "c-header";
 
 (function() {
-  let accordionElements = Array.from(document.querySelectorAll('.c-accordion__title'));
+  let accordionElements = Array.from(document.querySelectorAll("." + ACCORDION_TITLE));
 
+  // Remove/Add active class on target element
   const handleClickAccordionElement = (e) => {
     e.preventDefault();
     
     // Remove active class from all elements
     accordionElements.forEach(element => {
-      element.classList.remove(ACTIVE_CLASS);
+      element.classList.remove(ACCORDION_TITLE_ACTIVE);
     });
 
     // Add active class to selected element
-    e.currentTarget.classList.add(ACTIVE_CLASS);
+    e.currentTarget.classList.add(ACCORDION_TITLE_ACTIVE);
 
     // Change header color depending on accordion title color
     changeHeaderColor(e.currentTarget);
   }
   
+  // Assign click function to all elements
   accordionElements.forEach(element => {
-    element.addEventListener('click', handleClickAccordionElement)
+    element.addEventListener("click", handleClickAccordionElement)
   });
 })();
 
+// Change header color depending on clicked accordion title
 const changeHeaderColor = () => {
-  let elementStyles = getComputedStyle(document.querySelector("." + ACTIVE_CLASS)),
+  let elementStyles = getComputedStyle(document.querySelector("." + ACCORDION_TITLE_ACTIVE)),
       newStyles = {
         backgroundColor: elementStyles.backgroundColor,
         color: elementStyles.color
       };
 
-  setStylesOnElement(newStyles, document.getElementsByClassName("c-header")[0]);
+  setStylesOnElement(newStyles, document.getElementsByClassName(HEADER)[0]);
 };
 
+// Helper function to add multiple styles on element
 const setStylesOnElement = function(styles, element){
   Object.assign(element.style, styles);
 }
